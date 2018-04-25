9fb3f0077c362a298425f6e7ca276c7548ab5c71
Tom Söderlund <tom@tomsoderlund.com>
2018-04-25 21:33:09 +0200

    01 Basics

---
+++ 01-basics/script.js
@@ -0,0 +1,71 @@
+var FRAME_RATE = 10;
+
+var isRendering = false;
+var frameCount = 0;
+
+var capturer = new CCapture({
+	format: 'webm',
+	framerate: 10,
+	// verbose: true,
+});
+
+var start = function () {
+	isRendering = true;
+	frameCount = 0;
+	var canvas = document.getElementById('canvasElement');
+	initCanvas(canvas, canvas.getContext('2d'));
+	capturer.start();
+	render();
+};
+
+var pause = function () {
+	isRendering = !isRendering;
+	if (isRendering) {
+		capturer.start();
+		render();
+	}
+	else {
+		capturer.stop();
+	}
+};
+
+var download = function () {
+	isRendering = false;
+	capturer.stop();
+	capturer.save();
+};
+
+function render () {
+	frameCount++;
+	//document.getElementById('status').innerHTML = 'Rendering ' + frameCount + ' (' + frameCount/FRAME_RATE + ' s)';
+	if (isRendering) requestAnimationFrame(render);
+	// rendering stuff ...
+	var canvas = document.getElementById('canvasElement');
+	updateCanvas(canvas, canvas.getContext('2d'));
+	capturer.capture(canvas);
+}
+
+//----- Draw -----
+
+const getRandomPosition = (maxValue) => ({
+	x: Math.floor(Math.random() * maxValue),
+	y: Math.floor(Math.random() * maxValue),
+});
+
+let lastPosition;
+
+const initCanvas = function (canvas, context) {
+	context.fillStyle = 'white';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+const updateCanvas = function (canvas, context) {
+	context.strokeStyle = 'blue';
+	context.lineWidth = 5;
+	//context.strokeRect(20,20,150,100);
+	if (!lastPosition) lastPosition = getRandomPosition(canvas.width);
+	context.moveTo(lastPosition.x, lastPosition.y);
+	lastPosition = getRandomPosition(canvas.width);
+	context.lineTo(lastPosition.x, lastPosition.y);
+	context.stroke();
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Programmatic Animations with CCapture
+
+Making movies with JavaScript and HTML Canvas. Export as WebM video files.
+
+These examples use [CCapture](https://github.com/spite/ccapture.js) for capturing video frames.

af7597789ce08b437c050d404798d7d7fbcfc6a0
Tom Söderlund <tom@tomsoderlund.com>
2018-04-26 15:03:55 +0200

    Nicer boxes

--- 02-basic-physics/script.js
+++ 02-basic-physics/script.js
@@ -1,6 +1,6 @@
 //----- Rendering and Capturing -----
 
-var FRAME_RATE = 25;
+var FRAME_RATE = 30;
 var CANVAS_SIZE = 1080;
 var BOX_SIZE = 300;
 
@@ -72,7 +72,7 @@ var engine = Engine.create();
 
 // create two boxes and a ground
 var boxA = Bodies.rectangle(CANVAS_SIZE/2,-CANVAS_SIZE/2 - BOX_SIZE/2, BOX_SIZE,BOX_SIZE);
-boxA.color = 'darkslategray';
+boxA.color = 'dodgerblue';
 var boxB = Bodies.rectangle(CANVAS_SIZE/2 + BOX_SIZE*2/3,-CANVAS_SIZE/2 - BOX_SIZE*2, BOX_SIZE,BOX_SIZE);
 boxB.color = 'tomato';
 var ground = Bodies.rectangle(CANVAS_SIZE/2,CANVAS_SIZE-(100/2), CANVAS_SIZE-100,100, { isStatic: true });
@@ -97,26 +97,37 @@ const initCanvas = function (canvas, context) {
 	context.fillRect(0, 0, canvas.width, canvas.height);
 };
 
-const clearCanvas = function (canvas, context, color = 'white') {
+const fillCanvas = function (canvas, context, color = 'white') {
 	context.fillStyle = color;
 	context.fillRect(0, 0, canvas.width, canvas.height);
 };
 
+const fillCanvasGradient = function (canvas, context, colorInner = 'red', colorOuter = 'blue') {
+	var gradient = context.createRadialGradient(canvas.width/2,canvas.height/2,10, canvas.width/2,canvas.height/2,canvas.width/2); // x0,y0,r0,x1,y1,r1
+	gradient.addColorStop(0, colorInner);
+	gradient.addColorStop(1, colorOuter);
+	context.fillStyle = gradient;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+// From https://stackoverflow.com/questions/18838202/fill-polygon-on-canvas/18838472#18838472
 const drawPolygon = function (context, body) {
 	context.beginPath();
-	context.moveTo(body.vertices[0].x, body.vertices[0].y);
-	for (var j = 1; j < body.vertices.length; j += 1) {
-		context.lineTo(body.vertices[j].x, body.vertices[j].y);
-	};
-	context.lineTo(body.vertices[0].x, body.vertices[0].y);
+	//context.strokeStyle = body.color || 'silver';
+	context.fillStyle = body.color || 'silver';
 	context.lineWidth = 10;
-	context.strokeStyle = body.color || 'gray';
-	context.stroke();
+	context.lineCap = 'round';
+	for (var i = 0; i < body.vertices.length; i++) {
+		context.lineTo(body.vertices[i].x, body.vertices[i].y);
+	}
+	context.lineTo(body.vertices[0].x, body.vertices[0].y);
+	context.closePath();
+	context.fill();
+	//context.stroke();
 };
 
 const updateCanvas = function (canvas, context, frameCount) {
-	clearCanvas(canvas, context);
-	// From https://github.com/liabru/matter-js/wiki/Rendering
+	fillCanvasGradient(canvas, context, '#ccc', '#444');
 	var bodies = Composite.allBodies(engine.world);
 	for (var i = 0; i < bodies.length; i += 1) {
 		drawPolygon(context, bodies[i]);
--- README.md
+++ README.md
@@ -14,3 +14,4 @@ These examples use [CCapture.js](https://github.com/spite/ccapture.js) for captu
 ## Learn more
 
 - For more information on [how to draw with HTML Canvas, see this example](https://codepen.io/tomsoderlund/pen/Gdjrdx).
+- You can use [VLC Media Player](https://www.videolan.org) to convert WebM to MP4 (File → Convert/Stream, MP4/MOV and MPEG-4 format).
